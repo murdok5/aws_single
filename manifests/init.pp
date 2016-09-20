8722178800ba275053d07fb127bf0163dbcfdbea
murdok5 <mike.smith@puppet.com>
2016-09-20 07:34:13 -0700

    first class creation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,20 @@
 class aws_single {
 
-  include ec2tags
+  ec2_instance { 'webserver':
+    ensure            => 'running',
+    availability_zone => $ec2_metadata['placement']['availability-zone'],
+    image_id          => 'ami-ca56b5aa',
+    instance_type     => 't2.small',
+    key_name          => 'mikesmith-TSE',
+    region            => 'us-west-2',
+    security_groups   => $::ec2_metadata['security-groups'],
+    subnet            => 'subnet-54fccc30',
+    tags              => {
+      'department'    => 'TSE',
+      'project'       => 'TSE Demo',
+      'created_by'    => 'mike.smith',
+    },
+    user_data         => template('linux.erb'),
+  }
 
 }
