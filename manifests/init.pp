d2511e59e6e61199c1171ccd17018953d22e6666
murdok5 <mike.smith@puppet.com>
2016-09-20 15:28:45 -0700

    update and hardcode

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,12 @@ class aws_single {
 
   ec2_instance { 'webserver':
     ensure            => 'running',
-    availability_zone => $ec2_metadata['placement']['availability-zone'],
+    availability_zone => 'us-west-2',
     image_id          => 'ami-ca56b5aa',
     instance_type     => 't2.small',
     key_name          => 'mikesmith-TSE',
     region            => 'us-west-2',
-    security_groups   => $::ec2_metadata['security-groups'],
+    security_groups   => 'TSEDemoTest-Mike-TSEAWSDemoSG-V2DLT2VQBFLQ',
     subnet            => 'subnet-54fccc30',
     tags              => {
       'department'    => 'TSE',
@@ -20,3 +20,5 @@ class aws_single {
   }
 
 }
+
+include aws_single
