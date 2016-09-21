6bfd5e4b493f62d344197cb727b25b752a56981c
murdok5 <mike.smith@puppet.com>
2016-09-20 17:32:42 -0700

    tested working correctly

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,17 @@
 class aws_single {
 
   $pp_role = 'webserver'
-
+  $pe_master_hostname = $::ec2_metadata['hostname']
+..
   ec2_instance { 'webserver':
     ensure            => 'running',
-    availability_zone => 'us-west-2',
+    availability_zone =>  $::ec2_metadata['placement']['availability-zone'],
     image_id          => 'ami-ca56b5aa',
     instance_type     => 't2.small',
     key_name          => 'mikesmith-TSE',
-    region            => 'us-west-2',
-    security_groups   => 'TSEDemoTest-Mike-TSEAWSDemoSG-V2DLT2VQBFLQ',
-    subnet            => 'subnet-54fccc30',
+    region            => $::ec2_region,
+    security_groups   => $::ec2_metadata['security-groups'],
+    subnet            => $::ec2_metadata['network']['interfaces']['macs']["$::macaddress"]['subnet-id'],
     tags              => {
       'department'    => 'TSE',
       'project'       => 'TSE Demo',
